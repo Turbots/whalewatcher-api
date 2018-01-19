f1856e4ff6ee684fcdd293cd97a452d200bf4f82
Dieter Hubau <Dieter.Hubau@nike.com>
2018-01-19 14:45:09 +0100

    Initial generated project files

---
+++ src/main/java/be/turbots/whalewatcher/WhalewatcherApiApplication.java
@@ -0,0 +1,12 @@
+package be.turbots.whalewatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhalewatcherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhalewatcherApiApplication.class, args);
+	}
+}
